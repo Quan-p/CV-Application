9055c6f07ae88d51725b558e8c8b83c6b37a9792
Quan-p <quanp155@gmail.com>
2021-11-12 18:13:29 -0600

    Set text input box for first name test

--- src/components/CVMain/General.js
+++ src/components/CVMain/General.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/function-component-definition */
 import React, { useState } from 'react';
 
 const GeneralInfo = () => {
@@ -11,6 +12,20 @@ const GeneralInfo = () => {
     const [edit, setEdit] = useState(true);
 
     const { firstNme, lastName, email, phone } = personalInfo;
+
+    return (
+        <section>
+            <form>
+                <p>First Name:</p>
+                <label>
+                    <input
+                        type='text'
+                        placeholder='First Name'
+                    />
+                </label>
+            </form>
+        </section>
+    );
 }
 
 export default GeneralInfo;
