95d4ef0625c86d2447213961441f002ec7122e7e
Quan-p <quanp155@gmail.com>
2021-11-15 16:23:29 -0600

    Set up display section for general info

---
+++ src/components/CVMain/Display.js
@@ -0,0 +1,25 @@
+function DisplaySection(props) {
+    if (props.firstName) {
+        return (
+            <div class='displaySection'>
+                <span className='material-icons editIcon' onClick={props.handleEdit}>
+						create
+				</span>
+                <p>
+                    <span>First Name: </span> {props.firstName}
+                </p>
+                <p>
+                    <span>Last Name: </span> {props.lastName}
+                </p>
+                <p>
+                    <span>Email: </span> {props.email}
+                </p>
+                <p>
+                    <span>Phone: </span> {props.phone}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default DisplaySection;
